a82f990f71536b9684d6c50aead7646dfb587e7e
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-10 22:00:30 +0200

    Create function capitalize

--- foundations/fundamentals-3/script.js
+++ foundations/fundamentals-3/script.js
@@ -12,3 +12,13 @@ function add7(num) {
 function multiply(a, b) {
     return a * b;
 }
+
+/*
+    Takes a string and returns it with only the first
+    letter capitalized. Also trims the string.
+*/
+function capitalize(str) {
+    let trim = str.trim();
+    let lowercase = trim.toLowerCase();
+    return lowercase.charAt(0).toUpperCase() + lowercase.substring(1);
+}
